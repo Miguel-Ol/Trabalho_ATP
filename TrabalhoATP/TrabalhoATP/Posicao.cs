7bff40c465bd16032610cd0ffc9e23f1569eec56
Fernanda Soares <163454026+fernandassp@users.noreply.github.com>
2024-06-19 13:39:41 -0300

    Update Posicao.cs

--- TrabalhoATP/TrabalhoATP/Posicao.cs
+++ TrabalhoATP/TrabalhoATP/Posicao.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace TrabalhoATP
 {
-    internal class Class1
+    internal class Posicao
     {
+        private int linha;
+        public int Linha
+        {
+            get { return linha; }
+            set { linha = value; }
+        }
+        private int coluna;
+        public int Coluna
+        {
+            get { return coluna; } 
+            set { coluna = value; }
+        }
     }
 }
