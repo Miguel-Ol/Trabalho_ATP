a6ca4a645260199a342a82138a95e32630e46ff9
Fernanda Soares <163454026+fernandassp@users.noreply.github.com>
2024-06-26 16:16:40 -0300

    Update JogadorHumano.cs

--- TrabalhoATP/TrabalhoATP/JogadorHumano.cs
+++ TrabalhoATP/TrabalhoATP/JogadorHumano.cs
@@ -1,40 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ConstrainedExecution;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TrabalhoATP
 {
     internal class JogadorHumano
     {
-        private char [,] tabuleiro;
+        private char[,] tabuleiro;
         private int pontuacao;
         private int numTirosDados;
-        private Posicao [] posTirosDados; 
+        private Posicao[] posTirosDados;
         private string nickname;
-        public JogadorHumano (int linha,int coluna, Posicao[] posTirosDados, string nickname)
+        public JogadorHumano(int linha, int coluna, string nickname)
         {
             tabuleiro = GerarTabuleiro(linha, coluna);
             pontuacao = 0;
             numTirosDados = 0;
-            this.posTirosDados = new Posicao[linha * coluna]; //acho que assim ele pegaria o limite de posições
+            posTirosDados = new Posicao[linha*coluna];
             this.nickname = nickname;
         }
-        public char[,] GerarTabuleiro( int linha, int coluna)
+        public char[,] GerarTabuleiro(int linha, int coluna)
         {
             char[,] tab = new char[linha, coluna];
             for (int i = 0; i < linha; i++)
             {
-                for(int j = 0; j < coluna; j++)
+                for (int j = 0; j < coluna; j++)
                 {
-                    tab[i, j] = 'A' ;
+                    tab[i, j] = 'A';
                 }
             }
             return tab;
-        } 
-        public string GerarNickname (string nomeCompleto) //nome fornecido no main
+        }
+        public string GerarNickname(string nomeCompleto) //nome fornecido no main
         {
             string[] nome = nomeCompleto.Split(' '); //tranformo em um vetor, fazendo o split dos espaços
             if (nome.Length == 0) //como, sempre o primeiro nome é SEMPRE a primeira posição
@@ -44,35 +40,35 @@ namespace TrabalhoATP
             string nickname = nome[0];
             for (int i = 1; i < nome.Length; i++)
             {
-                nickname += nome[i][0]; 
+                nickname += nome[i][0];
             }
             return nickname;
         }
         public Posicao EscolherAtaque()
         {
-            bool posicaoValida = false; 
-            Posicao posicaoTiro=new Posicao();
+            bool posicaoValida = false;
+            Posicao posicaoTiro = new Posicao();
             int linha, coluna;
             string letra;
             string[] posLinCol;
-            while (!posicaoValida) 
+            while (!posicaoValida)
             {
                 Console.WriteLine("Escolha a posição do tiro(insira nesse modelo: numero da linha,numero da coluna): ");
-                letra=Console.ReadLine();
+                letra = Console.ReadLine();
                 posLinCol = letra.Split(',');
-                linha=int.Parse(posLinCol[0]);
-                coluna=int.Parse(posLinCol[1]);
-                if(linha>=0 && linha<tabuleiro.GetLength(0) && coluna>=0 && coluna<tabuleiro.GetLength(1)) //olha se está no lim do tab
+                linha = int.Parse(posLinCol[0]);
+                coluna = int.Parse(posLinCol[1]);
+                if (linha >= 0 && linha < tabuleiro.GetLength(0) && coluna >= 0 && coluna < tabuleiro.GetLength(1)) //olha se está no lim do tab
                 {
                     posicaoTiro = new Posicao();
                     posicaoValida = true;
-                    for (int i = 0; i < numTirosDados; i++) 
+                    for (int i = 0; i < numTirosDados; i++)
                     {
                         if (posTirosDados[i].Equals(posicaoTiro)) //para comparar
                         {
                             Console.WriteLine("Posição já utilizada, tente novamente!!");
-                            posicaoValida=false;
-                            i=numTirosDados;
+                            posicaoValida = false;
+                            i = numTirosDados;
                         }
                     }
                 }
@@ -105,7 +101,7 @@ namespace TrabalhoATP
         }
         public void ImprimirTabuleiroJogador()
         {
-            Console.WriteLine($"Tabuleiro {nickname}: " );
+            Console.WriteLine($"Tabuleiro {nickname}: ");
             for (int linha = 0; linha < tabuleiro.GetLength(0); linha++)
             {
                 for (int coluna = 0; coluna < tabuleiro.GetLength(1); coluna++)
@@ -133,9 +129,9 @@ namespace TrabalhoATP
         }
         public bool AdicionarEmbarcacao(Embarcacao embarcacao, Posicao pos)
         {
-            if (tabuleiro[pos.Linha, pos.Coluna] == 'A') 
+            if (tabuleiro[pos.Linha, pos.Coluna] == 'A')
             {
-                if (tabuleiro.GetLength(1) - pos.Coluna >= embarcacao.Tamanho) 
+                if (tabuleiro.GetLength(1) - pos.Coluna >= embarcacao.Tamanho)
                 {
                     int cont = 1;
                     bool vazio = true;
@@ -149,49 +145,49 @@ namespace TrabalhoATP
                     }
                     if (vazio)
                     {
-                        tabuleiro[pos.Linha, pos.Coluna] = embarcacao.Nome[0]; 
+                        tabuleiro[pos.Linha, pos.Coluna] = embarcacao.Nome[0];
                         return true;
                     }
                     else
                     {
-                        return false; 
+                        return false;
                     }
                 }
                 else
                 {
-                    return false; 
+                    return false;
                 }
             }
             else
             {
-                return false; 
+                return false;
             }
         }
 
         public char[,] Tabuleiro
         {
             get { return tabuleiro; }
-            set { tabuleiro = value;}
+            set { tabuleiro = value; }
         }
-        public int Pontuacao 
-        { 
-            get { return pontuacao; } 
-            set { pontuacao = value; } 
-        }
-        public int NumTirosDados 
+        public int Pontuacao
         {
-            get { return numTirosDados; } 
+            get { return pontuacao; }
+            set { pontuacao = value; }
+        }
+        public int NumTirosDados
+        {
+            get { return numTirosDados; }
             set { numTirosDados = value; }
         }
-        public Posicao[] PosTirosDados 
-        { 
-            get { return posTirosDados; } 
-            set { posTirosDados= value; }
+        public Posicao[] PosTirosDados
+        {
+            get { return posTirosDados; }
+            set { posTirosDados = value; }
         }
-        public string Nickname 
-        { 
-            get { return nickname; } 
-            set {  nickname = value; } 
+        public string Nickname
+        {
+            get { return nickname; }
+            set { nickname = value; }
         }
 
 
